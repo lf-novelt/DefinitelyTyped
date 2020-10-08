05bc656eb75bc5ae6f1c835a27062fcfb09c642d
Louis Frament <lf@novel-t.ch>
2020-10-08 13:29:14 +0200

    declares as global function

--- types/codemirror/addon/fold/xml-fold.d.ts
+++ types/codemirror/addon/fold/xml-fold.d.ts
@@ -10,16 +10,15 @@ import * as CodeMirror from "codemirror";
 declare module "codemirror" {
 
 
-    interface Editor {
-        /** An extension of the existing CodeMirror typings added by xml-fold.js */
-        // https://github.com/codemirror/CodeMirror/blob/1cb6de23c7e2b965201972ac5c6dcd2317e9eacf/addon/fold/xml-fold.js#L151
-        findMatchingTag: (cm: CodeMirror.Editor, pos: CodeMirror.Position, range: CodeMirror.Range) => void;
-        
-        // https://github.com/codemirror/CodeMirror/blob/1cb6de23c7e2b965201972ac5c6dcd2317e9eacf/addon/fold/xml-fold.js#L168
-        findEnclosingTag: (cm: CodeMirror.Editor, pos: CodeMirror.Position, range: CodeMirror.Range, tag: string) => void;
+    /** An extension of the existing CodeMirror typings added by xml-fold.js */
+    // https://github.com/codemirror/CodeMirror/blob/1cb6de23c7e2b965201972ac5c6dcd2317e9eacf/addon/fold/xml-fold.js#L151
+    function findMatchingTag(cm: CodeMirror.Editor, pos: CodeMirror.Position, range: CodeMirror.Range): void;
 
-        // https://github.com/codemirror/CodeMirror/blob/1cb6de23c7e2b965201972ac5c6dcd2317e9eacf/addon/fold/xml-fold.js#L180
-        scanForClosingTag: (cm: CodeMirror.Editor, pos: CodeMirror.Position, name: string, end: number) => void;
-    }
+    // https://github.com/codemirror/CodeMirror/blob/1cb6de23c7e2b965201972ac5c6dcd2317e9eacf/addon/fold/xml-fold.js#L168
+    function findEnclosingTag(cm: CodeMirror.Editor, pos: CodeMirror.Position, range: CodeMirror.Range, tag: string): void;
+
+    // https://github.com/codemirror/CodeMirror/blob/1cb6de23c7e2b965201972ac5c6dcd2317e9eacf/addon/fold/xml-fold.js#L180
+    function scanForClosingTag(cm: CodeMirror.Editor, pos: CodeMirror.Position, name: string, end: number): void;
+
 
 }
